7740e5996a25f5383dadd2c236da6fdf45ea9385
Murat Artan <muratartan@hotmail.com>
2022-09-19 17:59:28 +0300

    set transaction ckeckings

--- src/blockchain.rs
+++ src/blockchain.rs
@@ -1,3 +1,5 @@
+use std::collections::HashSet;
+
 use super::*;
 
 #[derive(Debug)]
@@ -13,9 +15,17 @@ pub enum BlockValidationErr {
 }
 pub struct Blockchain {
     pub blocks: Vec<Block>,
+    unspent_outputs: HashSet<Hash>,
 }
 
 impl Blockchain {
+    pub fn new() -> Self {
+        Blockchain {
+            blocks: vec![],
+            unspent_outputs: HashSet::new(),
+        }
+    }
+
     pub fn update_with_block(&mut self, block: Block) -> Result<(), BlockValidationErr> {
         let i = self.blocks.len();
 
@@ -37,6 +47,53 @@ impl Blockchain {
                 return Err(BlockValidationErr::InvalidGenesisBlockFormat);
             }
         }
+
+        if let Some((coinbase, transactions)) = block.transactions.split_first() {
+            if !coinbase.is_coinbase() {
+                return Err(BlockValidationErr::InvalidCoinbaseTransaction);
+            }
+
+            let mut block_spent: HashSet<Hash> = HashSet::new();
+            let mut block_created: HashSet<Hash> = HashSet::new();
+            let mut total_fee = 0;
+
+            for transaction in transactions {
+                let input_hashes = transaction.input_hashes();
+
+                if !(&input_hashes - &self.unspent_outputs).is_empty()
+                    || !(&input_hashes & &block_spent).is_empty()
+                {
+                    return Err(BlockValidationErr::InvalidInput);
+                }
+
+                let input_value = transaction.input_value();
+                let output_value = transaction.output_value();
+
+                if output_value > input_value {
+                    return Err(BlockValidationErr::InsufficientInputValue);
+                }
+
+                let fee = input_value - output_value;
+
+                total_fee += fee;
+
+                block_spent.extend(input_hashes);
+                block_created.extend(transaction.output_hashes());
+            }
+
+            if coinbase.output_value() < total_fee {
+                return Err(BlockValidationErr::InvalidCoinbaseTransaction);
+            } else {
+                block_created.extend(coinbase.output_hashes());
+            }
+
+            self.unspent_outputs
+                .retain(|output| !block_spent.contains(output));
+            self.unspent_outputs.extend(block_created);
+        }
+
+        self.blocks.push(block);
+
         Ok(())
     }
 }
