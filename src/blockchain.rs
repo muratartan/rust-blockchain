ff167ba8be3723e236738756c64205a81f8e7859
Murat Artan <muratartan@hotmail.com>
2022-09-19 17:40:31 +0300

    impl error enum

--- src/blockchain.rs
+++ src/blockchain.rs
@@ -14,33 +14,29 @@ pub enum BlockValidationErr {
 pub struct Blockchain {
     pub blocks: Vec<Block>,
 }
+
 impl Blockchain {
-    pub fn update_with_block(&mut self, block: Block) -> bool {
-        for (i, block) in self.blocks.iter().enumerate() {
-            if block.index != i as u32 {
-                println!(" index mismatch {} != {}", &block.index, &i);
-                return false;
-            } else if !block::check_difficulty(&block.hash(), block.difficulty) {
-                println!("difficulty failed");
-                return false;
-            } else if i != 0 {
-                // not genesis block
-                let prev_block = &self.blocks[i - 1];
-                if block.timestamp <= prev_block.timestamp {
-                    println!("time did not increase");
-                    return false;
-                } else if block.prev_block_hash != prev_block.hash {
-                    println!("hash mismatch");
-                    return false;
-                }
-            } else {
-                // genesis block
-                if block.prev_block_hash != vec![0; 32] {
-                    println!("genesis block prev_block_hash invalid");
-                    return false;
-                }
+    pub fn update_with_block(&mut self, block: Block) -> Result<(), BlockValidationErr> {
+        let i = self.blocks.len();
+
+        if block.index != i as u32 {
+            return Err(BlockValidationErr::MismatchedIndex);
+        } else if !block::check_difficulty(&block.hash(), block.difficulty) {
+            return Err(BlockValidationErr::InvalidHash);
+        } else if i != 0 {
+            // Not genesis block
+            let prev_block = &self.blocks[i - 1];
+            if block.timestamp <= prev_block.timestamp {
+                return Err(BlockValidationErr::AchronologicalTimestamp);
+            } else if block.prev_block_hash != prev_block.hash {
+                return Err(BlockValidationErr::MismatchedPreviousHash);
+            }
+        } else {
+            // Genesis block
+            if block.prev_block_hash != vec![0; 32] {
+                return Err(BlockValidationErr::InvalidGenesisBlockFormat);
             }
         }
-        true
+        Ok(())
     }
 }
