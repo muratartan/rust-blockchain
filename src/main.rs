03ad8d9fc5100e19e7a72ade118ab3d1fc7a902a
Murat Artan <muratartan@hotmail.com>
2022-09-19 18:03:53 +0300

    set genesis block

--- src/main.rs
+++ src/main.rs
@@ -3,16 +3,27 @@ use blockchainlib::*;
 fn main() {
     let difficulty = 0x000fffffffffffffffffffffffffffff;
 
-    let mut block = Block::new(
+    let mut genesis_block = Block::new(
         0,
         now(),
         vec![0; 32],
-        0,
-        "Another block".to_owned(),
+        vec![Transaction {
+            inputs: vec![],
+            outputs: vec![
+                transaction::Output {
+                    to_addr: "Alice".to_owned(),
+                    value: 50,
+                },
+                transaction::Output {
+                    to_addr: "Bob".to_owned(),
+                    value: 7,
+                },
+            ],
+        }],
         difficulty,
     );
 
-    block.mine();
+    genesis_block.mine();
 
     println!(" mined genesis block {:?}", &block);
 
