4835db0a66b1c35e69bb80cadba0879229558e95
Murat Artan <muratartan@hotmail.com>
2022-09-19 13:55:46 +0300

    set blockchain

--- src/main.rs
+++ src/main.rs
@@ -3,16 +3,26 @@ use blockchainlib::*;
 fn main() {
     let difficulty = 0x000fffffffffffffffffffffffffffff;
 
+    let mut block = Block::new(0, 0, vec![0; 32], 0, "Another block".to_owned(), difficulty);
+
+    block.mine();
+
+    println!(" mined genesis block {:?}", &block);
+
+    let mut last_hash = block.hash.clone();
+
     let mut blockchain = Blockchain {
         blocks: vec![block],
     };
 
     for i in 1..=10 {
-        let mut block = Block::new(i, 0, vec![0; 32], 0, "Another block".to_owned(), difficulty);
+        let mut block = Block::new(i, 0, last_hash, 0, "Another block".to_owned(), difficulty);
 
         block.mine();
 
-        println!(" mined genesis block {:?}", &block);
+        println!(" mined block {:?}", &block);
+
+        last_hash = block.hash.clone();
 
         blockchain.blocks.push(block);
     }
