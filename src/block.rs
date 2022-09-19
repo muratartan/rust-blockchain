eb59b8345cf793f36a04f4519d5c034c50cbadee
Murat Artan <muratartan@hotmail.com>
2022-09-19 11:28:11 +0300

    set difficulty field

--- src/block.rs
+++ src/block.rs
@@ -8,6 +8,7 @@ pub struct Block {
     pub prev_block_hash: BlockHash,
     pub nonce: u64,
     pub payload: String,
+    pub difficulty: u128,
 }
 
 impl Debug for Block {
@@ -30,6 +31,7 @@ impl Block {
         prev_block_hash: BlockHash,
         nonce: u64,
         payload: String,
+        difficulty: u128,
     ) -> Self {
         Block {
             index,
@@ -38,6 +40,7 @@ impl Block {
             prev_block_hash,
             nonce,
             payload,
+            difficulty,
         }
     }
 }
@@ -51,7 +54,12 @@ impl Hashable for Block {
         bytes.extend(&self.prev_block_hash);
         bytes.extend(&u64_bytes(&self.nonce));
         bytes.extend(self.payload.as_bytes());
+        bytes.extend(&u128_bytes(&self.difficulty));
 
         bytes
     }
+}
+
+pub fn check_difficulty(hash: BlockHash, difficulty: u128) -> bool {
+    difficulty > difficulty_bytes_as_u128(&hash)
 }
